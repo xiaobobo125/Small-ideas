245f0b6f147ac9841a2704d665b4effb2169f0f1
bolife <1259892859@qq.com>
2020-05-20 22:44:31 +0800

    finall

--- romatic520/js/index.js
+++ romatic520/js/index.js
@@ -1,5 +1,19 @@
-$(function(){
-    alert("执行了。。");
-    var cude = $(".cube");
-    setTimeout(cude.css("opacity",1),2000);
-  });
+function setHW(){
+  var height = $(window).height() - 800;
+  var width = $(window).width() - 800;
+  height = height/2;
+  width = width/2;
+  if(height < 0 || width < 0){
+      return;
+  }
+  var show = $(".rightMapper");
+  show.css("left",width+"px");
+  show.css("top",height+"px")
+}
+
+$(document).ready(function() {
+  setHW();
+  $(window).resize(function() {
+    setHW();
+});
+});
